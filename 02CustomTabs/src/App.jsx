69c456023f0c8b17923e7834667a194cfafd3652
Belal-Bari <tanvirj9@gmail.com>
2024-03-25 12:16:11 +0600

    Custom Tabs App

--- 02CustomTabs/src/App.jsx
+++ 02CustomTabs/src/App.jsx
@@ -1,27 +1,25 @@
-import { useState } from 'react'
-import Tabs from './Tabs'
+import Tabs from "./Tabs";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const tabs = [{
-    label: 'Tab 1',
-    content: <div>Tab 1 content</div>
-  }, 
-  {
-    label: 'Tab 2',
-    content: <div>Tab 2 content</div>
-  }, 
-  {
-    label: 'Tab 3',
-    content: <div>Tab 3 content</div>
-  }]
+  const tabs = [
+    {
+      label: "Tab 1",
+      content: <div>Tab 1 content</div>,
+    },
+    {
+      label: "Tab 2",
+      content: <div>Tab 2 content</div>,
+    },
+    {
+      label: "Tab 3",
+      content: <div>Tab 3 content</div>,
+    },
+  ];
 
-  function handleChange(currentTab){
-    console.log(currentTab)
+  function handleChange(currentTab) {
+    console.log(currentTab);
   }
-  return (
-    <Tabs tabsContent={tabs} onChange={handleChange} />
-  )
+  return <Tabs tabsContent={tabs} onChange={handleChange} />;
 }
 
-export default App
+export default App;
